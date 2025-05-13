827bac9918302301840978a74b0e7df17a4cd88e
Behrouz Babaki <Behrouz.Babaki@gmail.com>
2025-05-13 16:40:57 -0400

    testing eslint github workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
+import App from './app.tsx'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
-import App from './app.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
